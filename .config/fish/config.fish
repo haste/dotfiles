f2198589a17e5d40cd9826fa411450d7fe094809
Trond A Ekseth <troeks@gmail.com>
2017-09-09 21:22:35 +0200

    Fix X.org init check

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ end
 
 switch (uname)
   case Linux
-    if test -z $DISPLAY -a (tty) = '/dev/tty1'
+    if test -z "$DISPLAY" -a (tty) = '/dev/tty1'
       exec xinit -- -bs -core -nolisten tcp vt1
     end
 end
