27d43384c46bde061b2d5a36a8a6745bf67e4479
Trond A Ekseth <troeks@gmail.com>
2017-09-28 21:33:24 +0200

    Set EDITOR env variable on Linux

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -2,23 +2,20 @@ set -x ANDROID_HOME $HOME/scm/android-sdk-linux
 set -x GOROOT $HOME/scm/go
 set -x GOPATH $HOME/dev/go
 
-set paths \
-  $HOME/usr/bin \
-  $HOME/.local/bin \
-  $HOME/.cargo/bin \
-  $HOME/.fzf/bin \
-  $GOROOT/bin \
-  $GOPATH/bin \
-  $ANDROID_HOME/platform-tools \
-  $ANDROID_HOME/tools
+set paths $HOME/usr/bin $HOME/.local/bin $HOME/.cargo/bin $HOME/.fzf/bin $GOROOT/bin $GOPATH/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools
 
 for i in $paths
-  if test -d $i
-    set PATH $i $PATH
-  end
+    if test -d $i
+        set PATH $i $PATH
+    end
 end
 
 set -x LC_ALL 'en_US.UTF-8'
 set -x LANG 'en_US.UTF-8'
 
 set -x FZF_DEFAULT_COMMAND 'ag -g ""'
+
+switch (uname)
+    case Linux
+        set -x EDITR /usr/bin/nvim
+end
