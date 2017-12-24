d9df1cd485cc74e39d6aa187a931b1f226734d88
Trond A Ekseth <troeks@gmail.com>
2017-12-24 13:10:15 +0100

    Remove user git install PATHs

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,6 +1,4 @@
 set -x ANDROID_HOME $HOME/scm/android-sdk-linux
-set -x GOROOT $HOME/scm/go
-set -x GOPATH $HOME/dev/go
 
 set paths $HOME/usr/bin $HOME/.local/bin $HOME/.cargo/bin $HOME/.fzf/bin $GOROOT/bin $GOPATH/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools
 
