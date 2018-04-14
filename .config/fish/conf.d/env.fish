1f1d8c7351b9a1f057e56958e6d2527c0c1d7f53
Trond A Ekseth <troeks@gmail.com>
2018-04-14 13:59:29 +0200

    Use fish_user_paths instead of PATH

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,10 +1,11 @@
-set -x ANDROID_HOME $HOME/scm/android-sdk-linux
+set -x ANDROID_HOME $HOME/opt/android-sdk
+set -x GOPATH $HOME/dev/go
 
-set paths $HOME/usr/bin $HOME/.local/bin $HOME/.cargo/bin $HOME/.fzf/bin $GOROOT/bin $GOPATH/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $HOME/.yarn/bin
+set paths $HOME/usr/bin $HOME/.local/bin $HOME/.cargo/bin $GOROOT/bin $GOPATH/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $HOME/.yarn/bin
 
 for i in $paths
     if test -d $i
-        set PATH $i $PATH
+        set -U fish_user_paths $i $fish_user_paths
     end
 end
 
