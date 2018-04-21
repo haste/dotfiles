d16848dc00f935c7c2238ee868c697ff21d84deb
Trond A Ekseth <troeks@gmail.com>
2018-04-21 13:00:44 +0200

    Correctly set fish_user_paths

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,13 +1,7 @@
 set -x ANDROID_HOME $HOME/opt/android-sdk
 set -x GOPATH $HOME/dev/go
 
-set paths $HOME/usr/bin $HOME/.local/bin $HOME/.cargo/bin $GOROOT/bin $GOPATH/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $HOME/.yarn/bin
-
-for i in $paths
-    if test -d $i
-        set -U fish_user_paths $i $fish_user_paths
-    end
-end
+set fish_user_paths $GOROOT/bin $GOPATH/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $HOME/.yarn/bin
 
 set -x LC_ALL 'en_US.UTF-8'
 set -x LANG 'en_US.UTF-8'
