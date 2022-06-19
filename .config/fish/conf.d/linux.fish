5fb68f4a47b150ca629a87cb2ce04b7392fe91d6
Trond A Ekseth <troeks@gmail.com>
2022-06-19 14:31:11 +0200

    Make sway setup work better for all hosts

--- .config/fish/conf.d/linux.fish
+++ .config/fish/conf.d/linux.fish
@@ -1,6 +1,16 @@
 switch (uname)
   case Linux
     if test -z "$DISPLAY" -a (tty) = '/dev/tty1'
-      exec sway
+      export XDG_CURRENT_DESKTOP=sway
+      export XDG_SESSION_DESKTOP=sway
+
+      switch $hostname
+        case triagia
+          export GDK_SCALE=2
+          exec sway --config ~/.config/sway/triagia
+
+        case '*'
+          exec sway
+      end
     end
 end
--- .config/fish/conf.d/virtualfish-loader.fish
+++ .config/fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,4 @@
 set -g VIRTUALFISH_VERSION 2.5.1
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python3
-source /home/haste/.local/lib/python3.9/site-packages/virtualfish/virtual.fish
+source $HOME/.local/lib/python3.9/site-packages/virtualfish/virtual.fish
 emit virtualfish_did_setup_plugins
