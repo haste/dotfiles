f33b541e45cbf954973c713bd55c534d566eb67a
Trond A Ekseth <troeks@gmail.com>
2022-07-27 12:29:03 +0200

    fish: Add dbus-launcher and wayland env vars

--- .config/fish/conf.d/linux.fish
+++ .config/fish/conf.d/linux.fish
@@ -1,8 +1,10 @@
 switch (uname)
   case Linux
     if test -z "$DISPLAY" -a (tty) = '/dev/tty1'
+      export SDL_VIDEODRIVER=wayland
       export XDG_CURRENT_DESKTOP=sway
       export XDG_SESSION_DESKTOP=sway
+      export (dbus-launch)
 
       switch $hostname
         case triagia
