fb6355ad5a5907f1224642af3a8d103746eec8d6
Trond A Ekseth <troeks@gmail.com>
2024-05-18 14:31:46 +0200

    mise: Replace asdf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,3 @@
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
-switch (uname)
-  case Linux
-    source ~/.asdf/asdf.fish
-  case Darwin
-    source /opt/homebrew/opt/asdf/libexec/asdf.fish
-end
-
+mise activate fish | source
