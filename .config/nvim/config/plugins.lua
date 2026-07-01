1b1fffaf96d0c1795a2f969edbd20c9b408c1771
Trond A Ekseth <troeks@gmail.com>
2026-07-01 16:01:14 +0200

    nvim: Update nvim-treesitter to (archived) main branch

--- .config/nvim/config/plugins.lua
+++ .config/nvim/config/plugins.lua
@@ -125,51 +125,40 @@ require("pckr").add({
   --  Nvim Treesitter configurations and abstraction layer
   {
     "nvim-treesitter/nvim-treesitter",
+    -- main branch = the 0.12 rewrite; repo is archived, so pin to its final commit.
+    branch = "main",
+    commit = "4916d6592ede8c07973490d9322f187e07dfefac",
     run = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = {
-          "c",
-          "cpp",
-          "css",
-          "elixir",
-          "fish",
-          "go",
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "php",
-          "python",
-          "query",
-          "sql",
-          "terraform",
-          "typescript",
-          "vim",
-          "vimdoc",
-        },
+      require("nvim-treesitter").install({
+        "c",
+        "cpp",
+        "css",
+        "elixir",
+        "fish",
+        "go",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "php",
+        "python",
+        "query",
+        "sql",
+        "terraform",
+        "typescript",
+        "vim",
+        "vimdoc",
+      })
 
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
-        highlight = {
-          enable = true,
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-
-        indent = {
-          enable = true,
-        },
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+          if lang and pcall(vim.treesitter.language.add, lang) then
+            vim.treesitter.start(args.buf, lang)
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
       })
     end,
   },
