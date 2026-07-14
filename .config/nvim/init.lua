4a568f7bec5104f5ef7ef8a7ab8d93065e5850dd
Trond A Ekseth <troeks@gmail.com>
2026-07-14 10:06:48 +0200

    nvim: Fix dynamic loader on OSX

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,12 @@
 local config = vim.fn.stdpath("config")
 
 for _, file in pairs({
-	config .. "/config/options.lua",
-	config .. "/config/keys.lua",
-	config .. "/config/plugins.lua",
-	table.unpack(vim.split(vim.fn.glob("~/.config/nvim/plugin.d/*.lua"), "\n")),
+  config .. "/config/options.lua",
+  config .. "/config/keys.lua",
+  config .. "/config/plugins.lua",
+  (table.unpack or unpack)(vim.split(vim.fn.glob("~/.config/nvim/plugin.d/*.lua"), "\n")),
 }) do
-	vim.cmd("source " .. file)
+  vim.cmd("source " .. file)
 end
 
 vim.cmd.colorscheme("dracula")
